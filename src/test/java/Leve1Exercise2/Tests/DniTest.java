743706e279f5317b028c53d334d7d201fd99e962
ArturViaderDev <viadermataixartur@outlook.es>
2026-02-24 12:27:21 +0100

    Fix: Level 1 Exercise 2 fixed.

--- src/test/java/Leve1Exercise2/Tests/DniTest.java
+++ src/test/java/Leve1Exercise2/Tests/DniTest.java
@@ -2,99 +2,51 @@ package Leve1Exercise2.Tests;
 import Level1Exercise2.CalculoDni;
 import Level1Exercise2.Exceptions.IncorrectNumberException;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
 
 import static org.junit.Assert.assertTrue;
 
 
 public class DniTest {
-    @Test
-    public void testdni()
-    {
+    @ParameterizedTest
+    @CsvSource({
+            "54564454,J",
+            "45676545,V",
+            "54564454,J",
+            "45676545,V",
+            "58574829,Q",
+            "59675848,W",
+            "58584848,F",
+            "54848594,B",
+            "58493939,V",
+            "69686867,H",
+            "48484939,L",
+            "68694409,X",
+            "-1,-",
+            "100000000,-"
+    })
+    public void dniTest(int number, char expectedletter) {
         boolean correct = true;
         char letter;
         try
         {
-            letter = CalculoDni.letterDni(54564454);
-            if(letter!='J')
-            {
-                correct=false;
+            letter = CalculoDni.letterDni(number);
+            correct = true;
+            if (letter != expectedletter) {
+                correct = false;
             }
-            letter = CalculoDni.letterDni(45676545);
-            if(letter!='V')
+        } catch (Exception ex) {
+            if(number<0 || number>=100000000)
             {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(54564454);
-            if(letter!='J')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(45676545);
-            if(letter!='V')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(58574829);
-            if(letter!='Q')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(59675848);
-            if(letter!='W')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(58584848);
-            if(letter!='F')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(54848594);
-            if(letter!='B')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(58493939);
-            if(letter!='V')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(69686867);
-            if(letter!='H')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(48484939);
-            if(letter!='L')
-            {
-                correct=false;
-            }
-            letter = CalculoDni.letterDni(68694409);
-            if(letter!='X')
-            {
-                correct=false;
-            }
-        }
-        catch(Exception ex)
-        {
-            correct = false;
-        }
-        if(correct)
-        {
-            correct = false;
-            try {
-                letter = CalculoDni.letterDni(-1);
-            } catch (IncorrectNumberException e) {
                 correct = true;
             }
-            if(correct)
+            else
             {
                 correct = false;
-                try {
-                    letter = CalculoDni.letterDni(100000000);
-                } catch (IncorrectNumberException e) {
-                    correct=true;
-                }
             }
         }
         assertTrue(correct);
